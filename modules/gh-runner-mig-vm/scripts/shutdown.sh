99563fe23e88b27db7850d0bff8fe50377a3d6c2
Raj <the.raj.saxena@gmail.com>
2021-10-20 09:47:37 +0200

    Update shutdown script

--- modules/gh-runner-mig-vm/scripts/shutdown.sh
+++ modules/gh-runner-mig-vm/scripts/shutdown.sh
@@ -16,7 +16,7 @@
 secretUri=$(curl -sS "http://metadata.google.internal/computeMetadata/v1/instance/attributes/secret-id" -H "Metadata-Flavor: Google")
 #secrets URI is of the form projects/$PROJECT_NUMBER/secrets/$SECRET_NAME/versions/$SECRET_VERSION
 #split into array based on `/` delimeter
-IFS="/" read -r -a secretsConfig <<< "$secretUri"
+IFS="/" read -r -a secretsConfig <<<"$secretUri"
 #get SECRET_NAME and SECRET_VERSION
 SECRET_NAME=${secretsConfig[3]}
 SECRET_VERSION=${secretsConfig[5]}
@@ -30,5 +30,12 @@ export $(echo "$secrets" | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")
 cd /runner || exit
 ./svc.sh stop
 ./svc.sh uninstall
+if [[ -z $REPO_NAME ]]; then
+    # Remove action runner from the organisation
+    POST_URL="https://api.github.com/orgs/${REPO_OWNER}/actions/runners/remove-token"
+else
+    # Remove action runner from the repo
+    POST_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token"
+fi
 #remove the runner configuration
-RUNNER_ALLOW_RUNASROOT=1  /runner/config.sh remove --unattended --token "$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/remove-token" --header "authorization: Bearer ${GITHUB_TOKEN}"  --header "content-type: application/json" | jq -r .token)"
+RUNNER_ALLOW_RUNASROOT=1 /runner/config.sh remove --unattended --token "$(curl -sS --request POST --url "$POST_URL" --header "authorization: Bearer ${GITHUB_TOKEN}" --header "content-type: application/json" | jq -r .token)"
