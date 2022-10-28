76bc923afe20d31d6525bd7464ea17280dbc3712
Andrew Peabody <andrewpeabody@google.com>
2022-10-28 10:46:37 -0700

    Update versions.tf

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -15,12 +15,12 @@
  */
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.90.0"
+      version = ">= 3.53.0, < 5.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
