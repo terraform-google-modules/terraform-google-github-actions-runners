5bb8d09d7c2f082913c1bf58416f2cebf552574a
Awais Malik <awmalik@google.com>
2023-10-06 16:02:54 -0700

    chore: upgraded versions.tf to include minor bumps from tpg v5

--- modules/gh-oidc/versions.tf
+++ modules/gh-oidc/versions.tf
@@ -20,11 +20,11 @@ terraform {
 
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.64, < 5.0.0"
+      version = ">= 3.64, < 6"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.64, < 5.0.0"
+      version = ">= 3.64, < 6"
     }
   }
 
