ede4426a9268ad50524d3b0d23cb1dbf52ab99d3
Awais Malik <awmalik@google.com>
2023-10-09 08:42:54 -0700

    fix: upgraded versions.tf to include minor bumps from tpg v5 (#124)

--- modules/gh-runner-mig-vm/versions.tf
+++ modules/gh-runner-mig-vm/versions.tf
@@ -20,11 +20,11 @@ terraform {
 
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.53, < 5.0.0"
+      version = ">= 3.53, < 6"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.53, < 5.0.0"
+      version = ">= 3.53, < 6"
     }
   }
 
