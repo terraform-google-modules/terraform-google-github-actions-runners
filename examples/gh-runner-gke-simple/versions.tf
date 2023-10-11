e7a90434619119d59c6016bb0074e8947beb22b2
Andrew Peabody <andrewpeabody@google.com>
2023-10-11 08:25:37 -0700

    Update versions.tf

--- examples/gh-runner-gke-simple/versions.tf
+++ examples/gh-runner-gke-simple/versions.tf
@@ -17,10 +17,10 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
     }
     google-beta = {
-      source  = "hashicorp/google-beta"
+      source = "hashicorp/google-beta"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
