b1ccadc18f741a11d75b32b88c8d2d98d6ac2b89
Andrew Peabody <andrewpeabody@google.com>
2023-10-11 08:16:53 -0700

    Update versions.tf

--- examples/gh-runner-mig-container-vm-dind/versions.tf
+++ examples/gh-runner-mig-container-vm-dind/versions.tf
@@ -18,11 +18,9 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.0"
     }
   }
   required_version = ">= 0.13"
