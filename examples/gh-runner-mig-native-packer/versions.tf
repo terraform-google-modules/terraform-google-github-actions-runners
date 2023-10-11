693c8353f2152927c75d12cf8ebef3a6332eade2
Andrew Peabody <andrewpeabody@google.com>
2023-10-11 08:33:14 -0700

    Update and rename providers.tf to versions.tf

--- examples/gh-runner-mig-native-packer/versions.tf
+++ examples/gh-runner-mig-native-packer/versions.tf
@@ -14,11 +14,14 @@
  * limitations under the License.
  */
 
-
-provider "google" {
-  version = "~> 4.0"
-}
-
-provider "google-beta" {
-  version = "~> 4.0"
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+    }
+  }
+  required_version = ">= 0.13"
 }
