1779a460f71e3e7c40771c38ecbe5e0a25797cee
bharathkkb <bharathkrishnakb@gmail.com>
2023-12-18 20:19:09 +0000

    chore: update examples to use registry

--- examples/gh-runner-mig-native-packer/main.tf
+++ examples/gh-runner-mig-native-packer/main.tf
@@ -15,7 +15,9 @@
  */
 
 module "runner-mig" {
-  source               = "../../modules/gh-runner-mig-vm"
+  source  = "terraform-google-modules/github-actions-runners/google//modules/gh-runner-mig-vm"
+  version = "~> 3.0"
+
   create_network       = true
   project_id           = var.project_id
   repo_name            = var.repo_name
