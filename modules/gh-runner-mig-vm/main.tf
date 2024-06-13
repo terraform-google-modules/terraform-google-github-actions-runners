b4efa3016ce51e7813f415b6d61f5598dcc95cc7
Alexander Jelinek <jelinek@walter-group.com>
2024-06-13 14:59:47 +0200

    update mig module to v11

--- modules/gh-runner-mig-vm/main.tf
+++ modules/gh-runner-mig-vm/main.tf
@@ -154,7 +154,7 @@ module "mig_template" {
  *****************************************/
 module "mig" {
   source             = "terraform-google-modules/vm/google//modules/mig"
-  version            = "~> 7.0"
+  version            = "~> 11.0"
   project_id         = var.project_id
   subnetwork_project = var.project_id
   hostname           = local.instance_name
