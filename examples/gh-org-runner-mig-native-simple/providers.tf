ca1a1ddf6703bb28135ec59ddbb725e476c4a091
Grant Sorbo <gtsorbo@google.com>
2024-08-27 15:41:17 +0000

    fix: example provider constraint

--- examples/gh-org-runner-mig-native-simple/providers.tf
+++ examples/gh-org-runner-mig-native-simple/providers.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
   }
   required_version = ">= 0.13"
